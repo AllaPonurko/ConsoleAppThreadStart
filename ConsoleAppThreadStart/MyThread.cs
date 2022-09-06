5f0bc9bb90d5c8e5e83f0f9eccabfafdcf6c167c
Alla Ponurko <ponurkoalla264@gmail.com>
2022-09-06 23:37:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppThreadStart/MyThread.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleAppThreadStart
+{
+    public class MyThread
+    {
+        static public void Method()
+        {
+           
+          Console.WriteLine("Поток запущен");
+        }
+        static public ThreadStart start = new ThreadStart(Method);
+        public Thread T;
+        public MyThread()
+        {
+            T = new Thread(start);
+            T.Start();
+            //T.Join();
+        }
+    }
+}
---
+++ ConsoleAppThreadStart/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleAppThreadStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Method()
+            {
+                Console.WriteLine("Запущен новый поток");
+            }
+            void NewMethod()
+            {
+                Console.WriteLine("Запущен дочерний поток");
+            }
+            ThreadStart start = new ThreadStart(Method);
+            
+            Thread my = new Thread(start);
+            my.Start();
+            Thread.Sleep(1000);
+            Thread T = new Thread(start);
+            T.Start();
+            Thread.Sleep(1000);
+            my.Join();
+            Console.WriteLine("Поток ожидает завершения работы!");
+            
+        }
+    }
+}
